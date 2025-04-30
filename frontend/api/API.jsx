0587a6a79af73850c4c9da836c74e3f233dde7d1
Siddhesh Kulkarni <siddheshsanjaykulkarni1111@gmail.com>
2025-04-30 21:45:47 +0530

    Update API.jsx

--- frontend/api/API.jsx
+++ frontend/api/API.jsx
@@ -1,2 +1,2 @@
 export const API_URL = "https://api.escuelajs.co/api/v1/products";
-export const BASE_URL = "http://localhost:3009/user/";
+export const BASE_URL = "https://e-comm-api-murex.vercel.app/user/";
