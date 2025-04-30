dec7cd2600b49bb465360f86c9eff43742893b0b
Siddhesh Kulkarni <siddheshsanjaykulkarni1111@gmail.com>
2025-04-30 21:40:15 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ require("./Models/db");
 const UserRouter = require("./Routes/UserRouter");
 const cors = require("cors");   
 const app = express();
-const PORT = process.env.PORT || 3009;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
